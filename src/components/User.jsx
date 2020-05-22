1d2e280403d3739a6010254688f8284de992ba48
mj305 <maria.j.beckles@gmail.com>
2020-05-22 12:43:53 -0400

    created new user component refactored with Hooks

--- src/components/User.jsx
+++ src/components/User.jsx
@@ -1,96 +1,52 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import { Link } from 'react-router';
 
-class User extends React.Component {
-    constructor() {
-        super();
-        this.state = {
-        user: []    
-        };
-    }
+const User = (props) => {
+  const {username} = props.params;
+  const [user, setUser] = useState({});
+  const [repos, setRepos] = useState([]);
 
-    componentDidMount() {
-        fetch(`https://api.github.com/users/${this.props.params.username}`)
+  useEffect(() => {
+    fetch(`https://api.github.com/users/${username}`)
         .then(response => response.json())
         .then(
             user => {
-                this.setState({
-                    user: [user]
-                });
+                setUser(user);
             }
         );
-        
-        fetch(`https://api.github.com/users/${this.props.params.username}/repos`)
+
+        fetch(`https://api.github.com/users/${username}/repos`)
         .then(response => response.json())
         .then(
             repos => {
-                this.setState({
-                    repos: repos
-                });
+                setRepos(repos);
             }
         );
-    }
-    
-    renderStat(stat) {
-        console.log(stat);
-        return (
-            <li key={stat.name} className="user-info__stat">
-                <Link to={stat.url}>
-                    <p className="user-info__stat-value">{stat.value}</p>
-                    <p className="user-info__stat-name">{stat.name}</p>
-                </Link>
-            </li>
-        );
-    }
+  }, [])
+  console.log(user);
+  console.log(repos);
+  return (
+    <div className="user-page">
+        <div className="user-info">
+            <Link className="user-info__text" to={`/user/${user.login}`}>
+                <img className="user-info__avatar" src={user.avatar_url} alt={`${user.login} avatar`}/>
+                <h2 className="user-info__title">{user.login} ({user.name})</h2>
+                <p className="user-info__bio">{user.bio}</p>
+            </Link>
 
-    render() {
-        
-        if (!this.state.user) {
-            return (<div className="user-page">LOADING...</div>);
-        }
-
-        const user = this.state.user;
-
-        const stats = [
-            {
-                name: 'Public Repos',
-                value: user.public_repos,
-                url: `/user/${this.props.params.username}/repos`
-            },
-            {
-                name: 'Followers',
-                value: user.followers,
-                url: `/user/${this.props.params.username}/followers`
-            },
-            {
-                name: 'Following',
-                value: user.following,
-                url: `/user/${this.props.params.username}/following`
-            }
-        ];
-
-        console.log(this.state.user);
-
-        return (
-            <div className="user-page">
-                <div className="user-info">
-                    <Link className="user-info__text" to={`/user/${user.login}`}>
-                        <img className="user-info__avatar" src={user.avatar_url} alt={`${user.login} avatar`}/>
-                        <h2 className="user-info__title">{user.login} ({user.name})</h2>
-                        <p className="user-info__bio">{user.bio}</p>
-                    </Link>
-
-                    <ul className="user-info__stats">
-                        {stats.map(this.renderStat)}
-                    </ul>
-                    </div>
-                    {this.state.repos.map(repo => {
-                  console.log(repo)
-                  return <div>{repo.name}:{repo.html_url}</div>
-                })}
+            {/* <ul className="user-info__stats">
+                {stats.map(this.renderStat)}
+            </ul> */}
             </div>
-        );
-    }
-};
+            {repos.map(repo => {
+          console.log(repo)
+          return <div>{repo.name}:{repo.html_url}</div>
+        })}
+    </div>
+);
+
+
+
+}
 
 export default User;
